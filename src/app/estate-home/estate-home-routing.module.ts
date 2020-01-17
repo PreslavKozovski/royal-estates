0cb45bd2dd42256851106506dfdcab236b1dba68
PreslavKozovski <preslav.kozovski@progress.com>
2020-01-17 18:59:07 +0200

    feat: add Overview, Map, Similar pages

--- src/app/estate-home/estate-home-routing.module.ts
+++ src/app/estate-home/estate-home-routing.module.ts
@@ -6,7 +6,41 @@ import { EstateHomePage } from './estate-home.page';
 const routes: Routes = [
   {
     path: '',
-    component: EstateHomePage
+    component: EstateHomePage,
+    children: [
+      {
+        path: 'overview',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./tabs/overview/overview.module').then( m => m.OverviewPageModule)
+          }
+        ]
+      },
+      {
+        path: 'map',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./tabs/map/map.module').then( m => m.MapPageModule)
+          }
+        ]
+      },
+      {
+        path: 'similar',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./tabs/similar/similar.module').then( m => m.SimilarPageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: 'overview',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 
